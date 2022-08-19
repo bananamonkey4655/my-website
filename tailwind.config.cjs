42b6d3f0f335df50a1629d2fc2c6d6aac8289687
Humphrey <vanillaqicecream@gmail.com>
2022-08-19 16:26:30 +0800

    add project

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    borderWidth: {
+      DEFAULT: "1px",
+    },
+    fontFamily: {
+      DEFAULT: [
+        "Inter",
+        "sans-serif",
+        "ui-sans-serif",
+        "system-ui",
+        "sans-serif",
+      ],
+      headline: ["Menlo", "Monaco", "Lucida Console", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
