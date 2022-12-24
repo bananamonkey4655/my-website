6e1d0df691d64341a7ea6240067bb6d80728b1b6
Humphrey <vanillaqicecream@gmail.com>
2022-12-24 18:24:18 +0800

    add toggle dark mode button

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,14 @@ import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    // Disable injecting a basic `base.css` import on every page.
-    // Useful if you need to define and/or import your own custom `base.css`.
-    config: {
-      applyBaseStyles: false
-    }
-  }), image()]
+  integrations: [
+    tailwind({
+      // Disable injecting a basic `base.css` import on every page.
+      // Useful if you need to define and/or import your own custom `base.css`.
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+    image(),
+  ],
 });
